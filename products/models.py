4d381a087e947d43a4eccc3679c2be4a4e155d6d
buthainasky-code <buthainasky@gmail.com>
2026-06-23 18:49:24 +0300

    Delete products directory

--- products/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Product(models.Model):
-    name = models.CharField(max_length=200)
-    stock = models.PositiveIntegerField()        # This is the critical field we protect
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-
-    def __str__(self):
-        return f"{self.name} (stock: {self.stock})"
--- products/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('<int:product_id>/', views.product_detail, name='product-detail'),
-]
